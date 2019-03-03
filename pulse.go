f5e4bcae794d64ffd662f61f4601a769d67cd73b
Sam Orozco <sam.orozco@apptreesoftware.com>
2019-03-02 18:01:31 -0800

    Updating to use interfaces instead of booleans

--- pulse.go
+++ pulse.go
@@ -9,13 +9,13 @@ func main() {
 // A pulse is meant to be a pub-sub signaler
 // basically I want to get multiple reads from a single channel write
 type Pulse struct {
-	signal      chan bool
-	subscribers []chan bool
+	signal      chan interface{}
+	subscribers []chan interface{}
 	len         int32
 }
 
 // Init new pulse with signal channel
-func NewPulse(signal chan bool) *Pulse {
+func NewPulse(signal chan interface{}) *Pulse {
 	pulse := &Pulse{
 		signal: signal,
 		len:    0,
@@ -27,9 +27,9 @@ func NewPulse(signal chan bool) *Pulse {
 // subscribes to the signal bit
 // if anything is written to the signal chan
 // the returned chan will be written to
-func (p *Pulse) Subscribe() chan bool {
+func (p *Pulse) Subscribe() chan interface{} {
 	// add new chan to subscribers
-	newChan := make(chan bool)
+	newChan := make(chan interface{})
 	p.subscribers = append(p.subscribers, newChan)
 	atomic.AddInt32(&p.len, 1)
 	return newChan
@@ -42,7 +42,7 @@ func (p *Pulse) listen() {
 				continue
 			}
 			// this will ensure we read the chans in order
-			subChan := make(chan chan bool)
+			subChan := make(chan chan interface{})
 			for _, sub := range p.subscribers {
 				go func() {
 					mySub := <-subChan
